3b07ddfe8851ba279d727f4c933c162b3ba7371c
risperss <68032740+risperss@users.noreply.github.com>
2020-07-08 17:33:27 -0400

    Update Rook.java

--- Rook.java
+++ Rook.java
@@ -20,49 +20,4 @@ public class Rook extends Piece
 
         setPossibleMoves(possibleMoves);
     }
-
-    //not the band
-    public ArrayList<Move> oneDirection(int rowDir, int colDir, Square currSquare, Board board, Player currPlayer)
-    {
-        ArrayList<Move> oneDirection = new ArrayList<Move>();
-        int row = currSquare.getROW();
-        int col = currSquare.getCOL();
-
-        int i = 1;
-        while (i < 8)
-        {
-            int dRow = row + (i * rowDir);
-            int dCol = col + (i * colDir);
-            
-            if (dRow >= 0 && dRow < 8 && dCol < 8 && dCol >= 0)
-            { 
-                Square destSquare = board.getSquare(row + (i * rowDir), col + (i * colDir));
-                Piece destPiece = destSquare.getPiece();
-            
-                if (destPiece == null)
-                {
-                    oneDirection.add(new Move(currPlayer, currSquare, destSquare, board, MoveType.NORMAL));
-                }
-                else if (destPiece.getColor() != getColor())
-                {
-                    oneDirection.add(new Move(currPlayer, currSquare, destSquare, board, MoveType.NORMAL));
-                    break;
-                    //break because you cannot go through a piece of diff colour,
-                    //but you can still land on it
-                }
-                else 
-                {
-                    break;
-                    //cannot go to a square with a piece the same colour
-                }
-            }
-            else 
-            {
-                break;
-            }
-            i++;
-        }
-
-        return oneDirection;
-    }
 }
